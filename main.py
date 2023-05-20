5b6c35536658ca39251d17d012a86bbe41cc0ae2
Tyler <tyler.le@edu.sait.ca>
2023-05-20 15:24:56 -0600

    Stops game when collision happens

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import pygame
 from sys import exit
 
-# This is needed for all pygames
+def display_score():
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time
+    score_surface = test_font.render(f'Score:{current_time}', False, (64,64,64))
+    score_rect = score_surface.get_rect(center = (400,50))
+    screen.blit(score_surface,score_rect)
+
 pygame.init() 
 screen = pygame.display.set_mode((800,400))
-pygame.display.set_caption('Blade Runner')
+pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf',50)
+game_active = True
+start_time = 0
 
 sky = pygame.image.load('graphics/Sky.png').convert()
 ground = pygame.image.load('graphics/Ground.png').convert()
 
-score_surface = test_font.render('Score:', False, (64,64,64))
-score_rect = score_surface.get_rect(center = (400,50))
+# score_surface = test_font.render('Score:', False, (64,64,64))
+# score_rect = score_surface.get_rect(center = (400,50))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_hitbox = snail_surface.get_rect(bottomright = (600,300))
@@ -26,37 +33,46 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEBUTTONDOWN and player_hitbox.bottom >=300:
-                if player_hitbox.collidepoint(event.pos):
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN and player_hitbox.bottom >=300:
+                    if player_hitbox.collidepoint(event.pos):
+                        player_gravity = -20
+                
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_hitbox.bottom >=300:
                     player_gravity = -20
-              
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and player_hitbox.bottom >=300:
-                player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_hitbox.left = 800
+                start_time = int(pygame.time.get_ticks() / 1000)
 
-    # Background
-    screen.blit(sky,(0,0))
-    screen.blit(ground,(0,300))
-    pygame.draw.rect(screen,'#c0e8ec', score_rect, )
-    pygame.draw.rect(screen,'#c0e8ec', score_rect, 10)
-    screen.blit(score_surface,score_rect)
+# Game screen and mechanics
+    if game_active:
+        # Background
+        screen.blit(sky,(0,0))
+        screen.blit(ground,(0,300))
+        display_score()
 
-    # Snail
-    snail_hitbox.x -= 4
-    if snail_hitbox.right <= 0: snail_hitbox.left = 800
-    screen.blit(snail_surface,snail_hitbox)
+        # Snail
+        snail_hitbox.x -= 4
+        if snail_hitbox.right <= 0: snail_hitbox.left = 800
+        screen.blit(snail_surface,snail_hitbox)
 
-    # Player
-    player_gravity += 1
-    player_hitbox.y += player_gravity
-    if player_hitbox.bottom >= 300: player_hitbox.bottom = 300
-    screen.blit(player_surface,player_hitbox)
+        # Player
+        player_gravity += 1
+        player_hitbox.y += player_gravity
+        if player_hitbox.bottom >= 300: player_hitbox.bottom = 300
+        screen.blit(player_surface,player_hitbox)
 
-    # Collision
-    if snail_hitbox.colliderect(player_hitbox):
-        pygame.quit()
-        exit()
+        # Collision
+        if snail_hitbox.colliderect(player_hitbox):
+            game_active = False
 
+# Start screen
+    else:
+        screen.fill('Blue')
+ 
     pygame.display.update()
     clock.tick(60)
 
