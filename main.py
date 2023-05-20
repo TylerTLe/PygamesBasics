a9c464b44d96c76a2baf03a8907d7ad3cbbda741
Tyler <tyler.le@edu.sait.ca>
2023-05-19 22:21:38 -0600

    Adds scoreboard

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ test_font = pygame.font.Font('font/Pixeltype.ttf',50)
 
 sky = pygame.image.load('graphics/Sky.png').convert()
 ground = pygame.image.load('graphics/Ground.png').convert()
-text_surface = test_font.render('My game', False, 'Black')
+
+score_surface = test_font.render('Score:', False, (64,64,64))
+score_rect = score_surface.get_rect(center = (400,50))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_x = 600
@@ -30,20 +32,21 @@ while True:
 
     screen.blit(sky,(0,0))
     screen.blit(ground,(0,300))
-    screen.blit(text_surface,(300,50))
+    pygame.draw.rect(screen,'#c0e8ec', score_rect, )
+    pygame.draw.rect(screen,'#c0e8ec', score_rect, 10)
+    screen.blit(score_surface,score_rect)
 
     snail_hitbox.x -= 4
     if snail_hitbox.right <= 0: snail_hitbox.left = 800
     screen.blit(snail_surface,snail_hitbox)
     screen.blit(player_surface,player_hitbox)
-    
+
     # if player_hitbox.collidedict(snail_hitbox):
     #     player_hitbox.collidepoint()
 
     # mouse_pos = pygame.mouse.get_pos()
     # if player_hitbox.collidepoint((mouse_pos)):
     #     pygame.mouse.get_pressed()
-        
 
     pygame.display.update()
     clock.tick(60)
