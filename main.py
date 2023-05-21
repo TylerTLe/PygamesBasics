19654351cbe4ed946a1798c2cac24199b82be291
Tyler <tyler.le@edu.sait.ca>
2023-05-20 22:33:16 -0600

    adds player animation

--- main.py
+++ main.py
@@ -28,6 +28,16 @@ def collisions(player,obstacles):
             if player.colliderect(obstacle_rect): return False
     return True
 
+def player_animation():
+    global player_surf, player_index
+
+    if player_hitbox.bottom < 300:
+        player_surf = player_jump
+    else:
+        player_index += 0.1
+        if player_index >= len(player_walk): player_index = 0
+        player_surf = player_walk[int(player_index)]
+
 pygame.init() 
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
@@ -50,8 +60,14 @@ fly_surface = pygame.image.load('graphics/fly/fly1.png').convert_alpha()
 obstacle_rect_list = []
 
 
-player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
-player_hitbox = player_surface.get_rect(midbottom = (80,300))
+player_walk_1 = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_walk_2 = pygame.image.load('graphics/player/player_walk_2.png').convert_alpha()
+player_walk = [player_walk_1, player_walk_2]
+player_index = 0
+player_jump = pygame.image.load('graphics/player/jump.png').convert_alpha()
+
+player_surf = player_walk[player_index]
+player_hitbox = player_surf.get_rect(midbottom = (80,300))
 player_gravity = 0
 
 # Intro Screen
@@ -108,7 +124,8 @@ while True:
         player_gravity += 1
         player_hitbox.y += player_gravity
         if player_hitbox.bottom >= 300: player_hitbox.bottom = 300
-        screen.blit(player_surface,player_hitbox)
+        player_animation()
+        screen.blit(player_surf,player_hitbox)
 
         # Obstacle movement
         obstacle_rect_list = obstacle_movement(obstacle_rect_list)
